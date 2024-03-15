945a1517e33b14ec4841cc8d8136d225b0442a08
Mitchell Mark-George <mitchellmmarkgeorge@gmail.com>
2024-03-15 14:18:13 -0400

    inital commit

---
+++ webpack/webpack.main.config.ts
@@ -0,0 +1,30 @@
+import type { Configuration } from 'webpack';
+
+import { rules } from './webpack.rules';
+import { plugins } from './webpack.plugins';
+import CopyPlugin from 'copy-webpack-plugin';
+
+export const mainConfig: Configuration = {
+  /**
+   * This is the main entry point for your application, it's the first file
+   * that runs in the main process.
+   */
+  // devtool: 'source-map',
+  entry: './src/main/main.ts', // use absolute for this
+  // Put your normal webpack config below here
+  module: {
+    rules,
+  },
+  plugins: [
+    ...plugins,
+    new CopyPlugin({
+      patterns: [{
+        from: '../assets/icons',
+        to: '../assets/icons',
+      }]
+    }),
+  ],
+  resolve: {
+    extensions: ['.js', '.ts', '.jsx', '.tsx', '.css', '.json'],
+  },
+};
---
+++ webpack/webpack.renderer.config.ts
@@ -0,0 +1,32 @@
+import type { Configuration } from "webpack";
+
+import { rules } from "./webpack.rules";
+import { plugins } from "./webpack.plugins";
+
+export const rendererConfig: Configuration = {
+  // devtool: "source-map",
+  module: {
+    rules: [
+      ...rules,
+      {
+        test: /\.css$/,
+        use: [{ loader: "style-loader" }, { loader: "css-loader" }],
+      },
+      {
+        test: /\.svg$/,
+        use: ['@svgr/webpack'],
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+        generator: {
+          filename: 'assets/[name][ext]'
+        }
+      },
+    ],
+  },
+  plugins,
+  resolve: {
+    extensions: [".js", ".ts", ".jsx", ".tsx"],
+  },
+};
