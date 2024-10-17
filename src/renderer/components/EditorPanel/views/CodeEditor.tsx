f64af8f41da8ff412f1d2553dc465bf0ed9ed978
Mitchell Mark-George <mitchell.markgeorge@shopify.com>
2024-10-17 12:56:40 -0400

    implemented monaco

--- src/renderer/components/EditorPanel/views/CodeEditor.tsx
+++ src/renderer/components/EditorPanel/views/CodeEditor.tsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import { useEffect, useRef } from "react"
+import monaco from "monaco-editor";
+import "./CodeEditor.scss";
 
 export default function CodeEditor() {
+  const containerElement = useRef<HTMLDivElement | null>(null);
+  const editor = useRef<monaco.editor.IStandaloneCodeEditor | null>(null);
+
+  useEffect(() => {
+    if (containerElement.current) {
+      editor.current = monaco.editor.create(containerElement.current, {
+        automaticLayout: true,
+        value: "CodeEditor",
+        fontSize: 14,
+        theme: "vs-dark",
+        language: "javascript",
+      });
+    }
+  }, [])
+
   return (
-    <div>CodeEditor</div>
+    <div className="CodeEditor" ref={containerElement}/>
   )
 }
